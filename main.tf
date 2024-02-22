29051e9a17708c37d441655eb87d99b19e3ab9ae
Arpitha Sheshadri Bhat <144745793+arpithasheshadri@users.noreply.github.com>
2024-02-22 00:54:34 -0500

    modfied main.tf to add vm instance

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "google_compute_network" "vpc_network" {
   name                            = var.vpc_name
   delete_default_routes_on_create = true
   auto_create_subnetworks         = false
-  routing_mode                    = "REGIONAL"
+  routing_mode                    = var.vpc_regional
 }
 
 resource "google_compute_subnetwork" "webapp_subnet" {
@@ -25,16 +25,18 @@ resource "google_compute_route" "webapp_route" {
   dest_range       = var.webapp_route_range 
   network          = google_compute_network.vpc_network.self_link
   next_hop_gateway = "default-internet-gateway"
-  priority         = 100
+  priority         = 1000
+  
 }
 
 resource "google_compute_firewall" "webapp_allow_firewall" {
-  name          = "webapp-allow-firewall"
+  name          = "webapp-apply-firewall"
   network       = google_compute_network.vpc_network.self_link
   allow {
     protocol = "tcp"
-    ports    = [var.webapp_port]
+    ports    = [var.webapp_port,"80"]
   }
+  target_tags = ["http-server"]
   source_ranges = ["0.0.0.0/0"]
 }
 
@@ -46,4 +48,30 @@ resource "google_compute_firewall" "webapp_deny_firewall" {
     ports    = ["22"]
   }
   source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_instance" "cloud_vpc_instance" {
+  name         = var.vm_name
+  machine_type = var.vm_machine_type
+  zone         = var.vm_zone
+  boot_disk {
+    initialize_params {
+      image = var.vm_image
+      size  = var.vm_disk_size_gb
+      type  = var.vm_disk_type
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+    subnetwork = google_compute_subnetwork.webapp_subnet.self_link
+    access_config {
+    
+  }
+  }
+  tags = ["http-server"]
+  depends_on = [
+    google_compute_subnetwork.webapp_subnet,
+    google_compute_firewall.webapp_allow_firewall
+  ]
 }
